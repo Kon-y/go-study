2f8c1e2d09f31af91058552ff6cd496d1b279add
kon-y <kon@rosso-tokyo.co.jp>
2018-11-27 20:53:48 +0900

    com

--- ex01-04.go
+++ ex01-04.go
@@ -1,8 +1,21 @@
 package main
 
-import "ftm"
+// packageをインポートする。
+import {
+	"ftm"
+	"os"
+	"os/exec"
+}
 
 // 引数で問題番号を指定する。
+ex01 := false
+
+if ex01 {
+	fmt.println("ex01 は 正解です")
+}
+elseif {
+	fmt.println("are you ok?")
+}
 
 // 問題番号を判定する。
 
