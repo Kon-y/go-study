cd2c63e532cd728f564f51cc619c79edde755396
kon-y <kon@rosso-tokyo.co.jp>
2018-12-26 19:00:03 +0900

    ああ

--- 03_study/ex03.go
+++ 03_study/ex03.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	url := ""
+	res, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
+	buf := bytes.NewBuffer(body)
+	html := buf.String()
+	fmt.Println(html)
+}
