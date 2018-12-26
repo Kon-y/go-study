c4759f8bad4f7d71705cd78dfa1dbb750603569e
kon-y <kon@rosso-tokyo.co.jp>
2018-12-26 19:53:50 +0900

    commit

--- 03_study/ex04.go
+++ 03_study/ex04.go
@@ -1,1 +1,17 @@
-作成中
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	req, _ := http.NewRequest("GET", "http://golang.jp/", nil)
+
+	client := new(http.Client)
+	resp, _ := client.Do(req)
+	defer resp.Body.Close()
+
+	code := resp.StatusCode
+	fmt.Printf("%d\n", code)
+}
