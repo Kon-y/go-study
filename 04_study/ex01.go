7e7937c12f2813b8b8f4e9b045fff0c7f84c7d83
Kon-y <kon@rosso-tokyo.co.jp>
2018-12-29 15:14:39 +0000

    i hava no idea

--- 04_study/ex01.go
+++ 04_study/ex01.go
@@ -8,5 +8,5 @@ func main() {
 	fs := http.FileServer(http.Dir("public/"))
 	http.Handle("/", http.StripPrefix("/", fs))
 
-	http.ListenAndServe("localhost:9999", nil)
+	http.ListenAndServe(":9999", nil)
 }
