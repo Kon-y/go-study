6ca927a0f4c5339df5fb3b624c72f7536ce1f357
Kon-y <kon@rosso-tokyo.co.jp>
2019-01-01 16:15:02 +0900

    20190101 a happy new year

--- 04_第4回目/ex03/ex03_client_modify.go
+++ 04_第4回目/ex03/ex03_client_modify.go
@@ -7,20 +7,7 @@ import (
 	"net/http"
 )
 
-// URLを引数で指定し、実行する場合
-func cli() {
-	urlarg := flag.String("url", "https://github.com/n-guitar/go_study", "引数のURL書かないとか、まじか・・・")
-	flag.Parse()
-	req, _ := http.NewRequest("GET", *urlarg, nil)
-	client := new(http.Client)
-	resp, _ := client.Do(req)
-	defer resp.Body.Close()
-
-	html, _ := ioutil.ReadAll(resp.Body)
-	fmt.Println(string(html))
-}
-
-/*　URLを指定せず、実行する場合
+//　URLを指定せず、実行する場合
 func cli() {
 	urlarg := flag.String("url", "https://github.com/n-guitar/go_study", "まじか・・・")
 	flag.Parse()
@@ -32,23 +19,6 @@ func cli() {
 	html, _ := ioutil.ReadAll(resp.Body)
 	fmt.Println(string(html))
 }
-*/
-
-/*　ソースコードがおかしくなるタイプ・・・
-func cli() {
-	res, err := http.Get("http://localhost:9999")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer res.Body.Close()
-	Body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("%q\n", Body[:])
-}
-*/
 
 func main() {
 	fs := http.FileServer(http.Dir("public/"))
