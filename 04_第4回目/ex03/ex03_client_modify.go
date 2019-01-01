7ea98e564d52f7bc3349731c834a4546dcadb476
Kon-y <kon@rosso-tokyo.co.jp>
2019-01-02 02:00:32 +0900

    ex03 modify 201901020200

--- 04_第4回目/ex03/ex03_client_modify.go
+++ 04_第4回目/ex03/ex03_client_modify.go
@@ -7,11 +7,14 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
+var lines int = 0
+
 func set(url string) {
 	doc, _ := goquery.NewDocument(url)
 	doc.Find("a").Each(func(_ int, s *goquery.Selection) {
-		url, _ := s.Attr("href")
-		fmt.Println(url)
+		//url, _ := s.Attr("href")
+		//fmt.Println(url)
+		lines += 1
 	})
 }
 
@@ -22,4 +25,5 @@ func main() {
 	go http.ListenAndServe(":9999", nil)
 	url := "http://localhost:9999"
 	set(url)
+	fmt.Printf("Number of [a] tags is %d\n", lines)
 }
