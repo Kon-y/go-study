8305e3b977eb08e38843a1f1d284e0661e62632e
Kon-y <kon@rosso-tokyo.co.jp>
2019-01-01 16:40:16 +0900

    ex03 modify

--- 04_第4回目/ex03/ex03_client_modify.go
+++ 04_第4回目/ex03/ex03_client_modify.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"time"
 )
 
 //　URLを指定せず、実行する場合
 func cli() {
-	urlarg := flag.String("url", "https://github.com/n-guitar/go_study", "まじか・・・")
-	flag.Parse()
+	//urlarg := flag.String("url", "https://github.com/n-guitar/go_study", "まじか・・・")
+	//flag.Parse()
 	req, _ := http.NewRequest("GET", "http://localhost:9999", nil)
 	client := new(http.Client)
 	resp, _ := client.Do(req)
@@ -26,4 +26,5 @@ func main() {
 
 	go http.ListenAndServe(":9999", nil)
 	go cli()
+	time.Sleep(time.Second)
 }
