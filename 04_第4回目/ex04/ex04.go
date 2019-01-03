6808c7b6ab6797ac1b3cb0cca0b645baf97541a4
Kon-y <kon@rosso-tokyo.co.jp>
2019-01-03 20:40:37 +0900

    ex04 modify 201901032040

--- 04_第4回目/ex04/ex04.go
+++ 04_第4回目/ex04/ex04.go
@@ -6,23 +6,20 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
-var lines int = 0
+//var lines int = 0
 
 func set(url string) {
 	doc, _ := goquery.NewDocument(url)
 	doc.Find("a").Each(func(_ int, s *goquery.Selection) {
-		//url, _ := s.Attr("href")
-		//fmt.Println(url)
-		lines += 1
+		url, _ := s.Attr("href")
+		fmt.Println(url)
+		//lines += 1
 	})
 }
-
+/*
 func main() {
-	//	fs := http.FileServer(http.Dir("public/"))
-	//	http.Handle("/", http.StripPrefix("/", fs))
-
-	//	go http.ListenAndServe(":9999", nil)
 	url := "https://github.com/trending"
 	set(url)
 	fmt.Printf("Number of [a] tags is %d\n", lines)
 }
+*/
