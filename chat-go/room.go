4316edd4e14f5680d913f1ab1a7cd1b6f9e18f14
Kon-y <kon@rosso-tokyo.co.jp>
2019-01-05 21:49:57 +0900

    new-item create 201901052149

---
+++ chat-go/room.go
@@ -0,0 +1,27 @@
+package main
+
+import "github.com/gorilla/websocket"
+
+type room struct {
+	// forwardは他のクライアントに転送するためのメッセージを保持するチャネルです。
+	forward chan []byte
+}
+
+func (c *client) read() {
+	for {
+		if _, msg, err := c.socket.ReadMessage(); err == nil {
+			c.room.forward <- msg
+		} else {
+			break
+		}
+	}
+	c.socket.Close()
+}
+func (c *client) write() {
+	for msg := range c.send {
+		if err := c.socket.WriteMessage(websocket.TextMessage, msg); err != nil {
+			break
+		}
+	}
+	c.socket.Close()
+}
