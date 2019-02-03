843eb485cbeb242bdf4970b88515a1f86c235e60
Kon-y <kon@rosso-tokyo.co.jp>
2019-02-03 22:02:52 +0900

    modify 201902032202

--- 05_第5回目/ex01/ex01.go
+++ 05_第5回目/ex01/ex01.go
@@ -8,22 +8,39 @@ import (
 )
 
 func main() {
-	db, _ := sql.Open("mysql", "go_user:go_user@tcp(localhost:3306)/go_apps")
-	defer db.Close()
 
-	rows, _ := db.Query("SELECT * FROM tasks")
+	ryo, _ := sql.Open("mysql", "go_user:go_user@tcp(localhost:3306)/go_apps")
+	defer ryo.Close()
 
-	takky, _ := rows.Columns()
-	takky1 := make([]sql.RawBytes, len(takky))
-	takky2 := make([]interface{}, len(takky1))
+	rows, _ := ryo.Query(
+		`SELECT * FROM "tasks"`,
+	)
 
+	defer rows.Close()
 	for rows.Next() {
-		err := rows.Scan(takky2...)
-		if err != nil {
-			fmt.Printf("Unable to Scan values from Select Query")
-		}
-		for i, col := range takky1 {
-			fmt.Println(takky[i], ":", string(col))
-		}
+		id, hoge := 0, ""
+
+		fmt.Println(id, hoge)
 	}
 }
+
+/*
+   fmt.Println(id, name)
+   	rows, _ := db.Query("SELECT * FROM tasks")
+   	//fmt.Println
+
+   	takky, _ := rows.Columns()
+   	takky1 := make([]sql.RawBytes, len(takky))
+   	//takky2 := make([]interface{}, len(takky1))
+
+   	for rows.Next() {
+
+   		// 不要なカラムデータを読み捨てる。
+   		//err := rows.Scan(takky2...)
+
+   		for i, tokuda := range takky1 {
+   			fmt.Println(takky[i], ":", string(tokuda))
+   		}
+   	}
+   }
+*/
