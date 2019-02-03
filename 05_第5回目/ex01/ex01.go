d7d60fe7634d145ff1fe1da1c07db7547272c826
Kon-y <kon@rosso-tokyo.co.jp>
2019-02-03 19:11:13 +0900

    modify 201902031911

--- 05_第5回目/ex01/ex01.go
+++ 05_第5回目/ex01/ex01.go
@@ -3,14 +3,9 @@ package main
 import (
 	"database/sql"
 	"fmt"
-    "os/exec"
 
 	_ "github.com/go-sql-driver/mysql"
 )
-
-cmd := exec.Command("docker build -t docker_sample:001 .").Run()
-
-//err := exec.Command("docker run --rm -d --name mysql_001 --hostname mysql_001 -e MYSQL_ROOT_PASSWORD=root docker_sample:001").Run()
 
 func main() {
 	db, _ := sql.Open("mysql", "go_user:go_user@tcp(localhost:3306)/go_apps")
@@ -21,10 +16,6 @@ func main() {
 	takky, _ := rows.Columns()
 	takky1 := make([]sql.RawBytes, len(takky))
 	takky2 := make([]interface{}, len(takky1))
-
-	for i := range takky {
-		takky2[i] = &takky[i]
-	}
 
 	for rows.Next() {
 		err := rows.Scan(takky2...)
