44300199520a6b58b658a544b596e47e934d3998
Kon-y <kon@rosso-tokyo.co.jp>
2019-02-12 19:59:24 +0900

    ex01 comp 201902121959

--- 06_第6回目/ex01/ex01.go
+++ 06_第6回目/ex01/ex01.go
@@ -2,29 +2,26 @@ package main
 
 import "fmt"
 
-func plus(a int, b int) int {
-	return a + b
-}
-
 func main() {
-	answer := plus(1, 2)
-	fmt.Println(answer)
 
 	var a = 1
 	// 足し算
 	a += 1
-	fmt.Println(a) // 2
+	fmt.Printf("足し算の問題です。1 + 1 = %v\n", a)
+	v := a
 
 	//　引き算
 	a -= 1
-	fmt.Println(a) // 1
+	fmt.Printf("引き算の問題です。%v - 1 = %v\n", v, a)
+	v = a
 
 	//　掛け算
 	a *= 2
-	fmt.Println(a) // 2
+	fmt.Printf("掛け算の問題です。%v × 2 = %v\n", v, a)
+	v = a
 
 	//　割り算
 	a /= 2
-	fmt.Println(a) // 1
+	fmt.Printf("割り算の問題です。%v ÷ 2 = %v\n", v, a)
 
 }
